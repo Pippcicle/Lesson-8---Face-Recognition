06275d16133221f098250656596f87572a5cd1c9
Pippcicle <pippaamsmith@gmail.com>
2026-02-10 18:53:33 +0000

    Add files via upload

--- colourimages.py
+++ colourimages.py
@@ -1,6 +1,6 @@
 import cv2, sys, numpy, os
 
-haar_file = 'haarcascade_frontalface_default.xml'
+haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 
 datasets = 'datasets'
 
@@ -15,3 +15,35 @@ if not os.path.isdir(path):
 face_cascade  = cv2.CascadeClassifier(haar_file)
 
 webcam = cv2.VideoCapture(0)
+
+count = 1
+while count < 30 : 
+    ret, im = webcam.read()
+
+    if not ret: 
+        continue 
+
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+
+    for (x,y,w,h) in faces : 
+        cv2.rectangle(im, (x,y), (x+w, y+h), (255,0,0), 2)
+
+        face = im[y:y+h, x:x+w]
+
+        face_resize = cv2.resize(face, (width, height))
+
+        cv2.imwrite('%s/%s.png' % (path, count), face_resize)
+
+        count += 1
+    
+    cv2.imshow('OpenCV - Face Capture', im)
+    
+    key = cv2.waitKey(10)
+
+    if key == 27 : 
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
